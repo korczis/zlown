8db13fe0f39777e6d39913326a7df3e928c5e947
Tomas Korcak <korczis@gmail.com>
2016-02-14 16:29:14 +0100

    Update daemon.rb

--- lib/zlown/daemon/daemon.rb
+++ lib/zlown/daemon/daemon.rb
@@ -1,4 +1,8 @@
-# encoding: utf-8
+# encoding: UTF-8
+#
+# Copyright (c) 2016 Tomas Korcak <korczis@gmail.com>. All rights reserved.
+# This source code is licensed under the MIT-style license found in the
+# LICENSE file in the root directory of this source tree.
 
 module Zlown
   class Daemon
