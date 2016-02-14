d5b4beeaa008808b0f19e2894f232b28a2962ea7
Tomas Korcak <korczis@gmail.com>
2016-02-14 17:00:08 +0100

    Add missing files

---
+++ lib/zlown/systemctl/systemctl.rb
@@ -0,0 +1,37 @@
+# encoding: UTF-8
+#
+# Copyright (c) 2016 Tomas Korcak <korczis@gmail.com>. All rights reserved.
+# This source code is licensed under the MIT-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+module Zlown
+  class Systemctl
+    SERVICE_FILE = File.expand_path('../../../../etc/systemd/system/zlown.service', __FILE__)
+    SERVICE_NAME = 'zlown'
+
+    def self.enable(args = [], opts = {})
+      puts 'Enabling systemctl service'
+      cmd = "systemctl enable #{SERVICE_FILE}"
+      system cmd
+    end
+
+    def self.disable(args = [], opts = {})
+      puts 'Disabling systemctl service'
+      cmd = "systemctl disable #{SERVICE_NAME}"
+      system cmd
+    end
+
+    def self.start(args = [], opts = {})
+      puts 'Starting systemctl service'
+      cmd = "systemctl start #{SERVICE_NAME}"
+      system cmd
+    end
+
+    def self.stop(args = [], opts = {})
+      puts 'Stopping systemctl service'
+      cmd = "systemctl stop #{SERVICE_NAME}"
+      system cmd
+    end
+  end
+end
+
